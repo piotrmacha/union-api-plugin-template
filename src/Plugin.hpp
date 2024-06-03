faf942ae7f46095f04f38af82af67d2420ba3c13
Piotr Macha <me@piotrmacha.pl>
2024-06-03 10:45:41 +0200

    Fix the example code

--- src/Plugin.hpp
+++ src/Plugin.hpp
@@ -7,6 +7,8 @@ namespace GOTHIC_NAMESPACE
     {
         constexpr const char* versions[] = {"", "Gothic I", "Gothic Sequel (:o)", "Gothic II Classic", "Gothic II Night of the Raven"};
         Union::String::Format("Hello World from MyPlugin Example!\nGame: {0}", versions[GetGameVersion()]).ShowMessage();
+        // Execute original method
+        (this->*CGameManager_Init_Ivk)(handle);
     }
 //@EXAMPLE END
 }
